c57e581d92833c44ecfacd62846fac8799d6ec24
SORSAI\punyanuchsai <pppunyanuch.sai@gmail.com>
2017-04-17 16:18:49 +0700

    Use an array to isplay the prices of each object, and add the object list

---
+++ Week3_JSandArray/js/shop.js
@@ -0,0 +1,27 @@
+var customerName ="Punyanuch";
+var products = ["Brooklyn T-Shirt White", "Brooklyn T-Shirt Black", "Apple Watch", "Android Phone"];
+var prices = [10, 10, 199, 159];
+
+var total = 0;
+var customer = document.getElementById("name");
+customer.textContent = customerName;
+
+// document.write("Total is " + total);
+
+var productsText="";
+var productsElement = document.getElementById("product-list");
+
+for ( var i = 0; i < products.length; i++) {
+	productsText += "<li class='list-group-item'><span class='badge'>$" + prices[0] + "</span>" + products[0] + "</li>";
+	total += prices[i];
+}
+
+ productsElement.innerHTML = productsText;
+
+
+ var totalPriceElement = document.getElementById("total-price");
+ totalPriceElement.textContent = total;
+
+// // var totalPrice = document.getElementById("total");
+// // totalPrice.textContent = total;
+
