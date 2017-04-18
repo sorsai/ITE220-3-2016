7680d82594589758f7a9ba3b039e45d87d7705b1
SORSAI\punyanuchsai <pppunyanuch.sai@gmail.com>
2017-04-18 15:28:46 +0700

    upload Week3-2

---
+++ Week4/js/shop.js
@@ -0,0 +1,59 @@
+var shop = {
+	customerName: "David",
+	total: 0,
+	products:  [
+	"Brooklyn T-Shirt White",
+	"Brooklyn T-Shirt Black", 
+	"Apple Watch", 
+	"Android Phone"
+	],
+	prices: [10, 10, 199, 159],
+	displayCustomerName: function() {
+		var el = document.get
+			getElementById("name");
+		el.textContent = shop.customerName;
+	}
+};
+
+shop.displayCustomerName();
+
+
+// var customerName ="Punyanuch";
+// var products = ["Brooklyn T-Shirt White", "Brooklyn T-Shirt Black", "Apple Watch", "Android Phone"];
+// var prices = [10, 10, 199, 159];
+
+// var total = 0;
+// var customer = document.getElementById("name");
+// customer.textContent = customerName;
+
+// // document.write("Total is " + total);
+
+// var productsText="";
+// var productsElement = document.getElementById("product-list");
+
+// for ( var i = 0; i < products.length; i++) {
+// 	productsText += "<li class='list-group-item'><span class='badge'>$" + prices[i] + "</span>" + products[i] + "</li>";
+// 	total += prices[i];
+// }
+
+//  productsElement.innerHTML = productsText;
+
+
+//  var totalPriceElement = document.getElementById("total-price");
+//  totalPriceElement.textContent = total;
+
+ var date = new Date();
+ 	if (date.getHours() < 12) {
+ 		document.getElementById('time').innerHTML = " Good Morning";
+ 	} else if (date.getHours() > 12) {
+ 		document.getElementById('time').innerHTML = " Good afternoon";
+ 	} else {
+ 		document.getElementById('time').innerHTML = "Good Evening";
+ 	}
+
+//  var dateElement = document.getElementById("date");
+//  dateElement.textContent = date;
+
+// // // var totalPrice = document.getElementById("total");
+// // // totalPrice.textContent = total;
+
---
+++ week3-2/js/shop.js
@@ -0,0 +1,84 @@
+var shop = {
+	customerName: "David",
+	total: 0,
+	products: [
+		"Brooklyn T-Shirt White", 
+		"Brooklyn T-Shirt Black", 
+		"Apple Watch", 
+		"Android Phone"
+	],
+	prices: [10,10,199,159],
+	displayCustomerName: function() {
+		var el = document.getElementById("name");
+		el.textContent = customerName;
+	},
+
+	displayProducts: function() {
+		var productsText = "";
+		var productsElement = document.getElementById("product-list");
+		 for(var i = 0; i<this.products.length;i++) {
+
+            productsText += "<li class='list-group-item'><span class='badge'>$"+this.prices[i]+"</span>"+this.products[i]+"</li>";
+            this.total += this.prices[i];
+        }
+        productsElement.innerHTML = productsText;
+
+        var totalPriceElement = document.getElementById("total-price");
+        totalPriceElement.textContent = total;
+        
+	}
+};
+
+shop.displayCustomerName();
+shop.displayProducts();
+// var products = [
+// 	"Brooklyn T-Shirt White", 
+// 	"Brooklyn T-Shirt Black", 
+// 	"Apple Watch", 
+// 	"Android Phone"
+// ];
+// var prices = [10,10,199,159];
+
+// var customerName = "Marina";
+// var total = 0;
+// var customer = document.getElementById("name");
+// customer.textContent = customerName;
+
+
+// var productsText = "";
+// var productsElement = document.getElementById("product-list");
+
+// for(var i = 0; i<products.length;i++) {
+
+// 	productsText += "<li class='list-group-item'><span class='badge'>$"+prices[i]+"</span>"+products[i]+"</li>";
+// 	total += prices[i];
+// }
+
+
+// productsElement.innerHTML = productsText;
+
+// var totalPriceElement = document.getElementById("total-price");
+// totalPriceElement.textContent = total;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
