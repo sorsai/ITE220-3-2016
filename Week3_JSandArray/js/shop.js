fa98c38f080251ae03c06481a7e55a2d43304d8a
SORSAI\punyanuchsai <pppunyanuch.sai@gmail.com>
2017-04-18 21:39:34 +0700

    Calculate the discount of the price and edit the time to say hello

--- Week3_JSandArray/js/shop.js
+++ Week3_JSandArray/js/shop.js
@@ -2,15 +2,16 @@ var customerName ="Punyanuch";
 var products = ["Brooklyn T-Shirt White", "Brooklyn T-Shirt Black", "Apple Watch", "Android Phone"];
 var prices = [10, 10, 199, 159];
 
-var total = 0;
+
 var customer = document.getElementById("name");
 customer.textContent = customerName;
+
 
 // document.write("Total is " + total);
 
 var productsText="";
 var productsElement = document.getElementById("product-list");
-
+var total = 0;
 for ( var i = 0; i < products.length; i++) {
 	productsText += "<li class='list-group-item'><span class='badge'>$" + prices[i] + "</span>" + products[i] + "</li>";
 	total += prices[i];
@@ -20,19 +21,37 @@ for ( var i = 0; i < products.length; i++) {
 
 
  var totalPriceElement = document.getElementById("total-price");
- totalPriceElement.textContent = total;
+ totalPriceElement.textContent = total-(total * 0.25);
 
- var date = new Date();
- 	if (date.getHours() < 12) {
- 		document.getElementById('time').innerHTML = " Good Morning";
- 	} else if (date.getHours() > 12) {
- 		document.getElementById('time').innerHTML = " Good afternoon";
- 	} else {
- 		document.getElementById('time').innerHTML = "Good Evening";
- 	}
+var date = new Date();
+var hours = date.getHours();
+var say;
+if (hours < 12 ){
+	say = "Good Morning";
+}else if (hours < 18){
+	say = "Good Afternoon";
+}else{
+	say = "Good Evening";
+}
+document.getElementById("time").innerHTML = say;
 
- var dateElement = document.getElementById("date");
- dateElement.textContent = date;
+ // another way for display about the time//
+ // var date = new Date();
+ // 	if (date.getHours() < 12) {
+ // 		document.getElementById('time').innerHTML = " Good Morning";
+ // 	} else if (date.getHours() < 18) {
+ // 		document.getElementById('time').innerHTML = " Good afternoon";
+ // 	} else {
+ // 		document.getElementById('time').innerHTML = "Good Evening";
+ // 	}
+
+ // var dateElement = document.getElementById("date");
+ // dateElement.textContent = date;
+ 
+
+ 
+
+
 
 // // var totalPrice = document.getElementById("total");
 // // totalPrice.textContent = total;
