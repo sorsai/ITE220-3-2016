cfdfa68d084f3191ef9e0a38a10f209f0f39da06
SORSAI\punyanuchsai <pppunyanuch.sai@gmail.com>
2017-04-25 21:16:19 +0700

    swap

--- Week4/js/calendar.js
+++ Week4/js/calendar.js
@@ -2,14 +2,14 @@ var calendar = {
 			header:"<tr><th>S</th><th>M</th><th>T</th><th>W</th><th>T</th><th>F</th><th>S</th></tr><tr>",
 			  displayCalendar: function() {
 			  	var ele = document.getElementById("calendar");
-			  	var calendarText = "<table class='table table-considered'>";
+			  	var calendarText = "<table class='table table-condensed'>";
 			  	calendarText += this.header;
 			//write code to display date num
 				for (var i = 1; i <= 31; i++) {
 					//code thatt display the date..
 					document.write("<tr>");
   					if (i % 7 === 0) { 
-  						calendarText += "<td>"+i+"</td></tr><tr>";
+  						calendarText += "<td>"+ i +"</td></tr><tr>";
   						// "| "+i+"|"
 					}else {
 						calendarText += "<td>" + i + "</td>";
