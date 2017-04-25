48885729a99c132d41eaad3b59ed3bb5b87b1edf
SORSAI\punyanuchsai <pppunyanuch.sai@gmail.com>
2017-04-25 20:53:34 +0700

    Edit the alignment of <th> in calendar.js

--- Week4/js/calendar.js
+++ Week4/js/calendar.js
@@ -1,18 +1,19 @@
 var calendar = {
-			header: "<tr><th>S</th><th>M</th><th>T</th><th>W</th><th>T</th><th>F</th><th>S</th></tr><tr>",
+			header:"<tr centerText><th>S</th><th>M</th><th>T</th><th>W</th><th>T</th><th>F</th><th>S</th></tr><tr>",
 			  displayCalendar: function() {
 			  	var ele = document.getElementById("calendar");
 			  	var calendarText = "<table class='table table-considered'>";
 			  	calendarText += this.header;
+			//write code to display date num
 				for (var i = 1; i <= 31; i++) {
-					document.write("<tr>");
 					//code thatt display the date..
-  					 if (i % 7 == 0) { 
+					document.write("<tr>");
+  					if (i % 7 === 0) { 
   						calendarText += "<td>"+i+"</td></tr><tr>";
   						// "| "+i+"|"
-				}	else {
+					}else {
 						calendarText += "<td>" + i + "</td>";
-				}
+					}
 			}
 			  	calendarText += "</table>"
 			  	ele.innerHTML = calendarText;
